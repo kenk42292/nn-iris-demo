174cb42b32ff82fe3474bf89be5944439fc307ad
Ken Katagiri <kenk42292@gmail.com>
2018-05-10 11:09:20 -0700

    Mostly done with the html

--- controllers/demo-controller.js
+++
@@ -1,6 +0,0 @@
-
-demoApp.controller("demoController", function($scope) {
-    // Hmm... probably don't need anything here.
-});
-
-
--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,28 @@
 /* global $, document */
 
-/* Import header on to page */
-$(document).ready(function() {
-    'use strict';
-    $(function() {
-        $('.header-wrapper').load('/content/widgets/header.html');
-    })
-})
+/* control buttons */
 
-/* Make navigation item 'active' on click */
 $(document).ready(function() {
     'use strict';
-    $('.navbar-nav li a').click(function(event) { //eslint-disable-line no-unused-vars
+    $('#next-input').click(function(event) { //eslint-disable-line no-unused-vars
         'use strict';
         // remove 'active' from all li
         $('.navbar-nav li a').parent().removeClass("active");
         // add 'active' to specific li
         $(this).parent().addClass("active");
     })
-})
-
-/* Extension of contains jquery selector to be case-insensitve */
-$.expr[":"].containsi = $.expr.createPseudo(function(arg) {
-    return function( elem ) {
-        return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
-    };
 });
+
+$(document).ready(function() {
+    'use strict';
+    $('#next-input').click(function(event) { //eslint-disable-line no-unused-vars
+        'use strict';
+        // remove 'active' from all li
+        $('.navbar-nav li a').parent().removeClass("active");
+        // add 'active' to specific li
+        $(this).parent().addClass("active");
+    })
+});
+
+
 
