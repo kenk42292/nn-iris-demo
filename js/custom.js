8d8c51c58b54b55cf23942081f346c9c530d5378
Ken Katagiri <kenk42292@gmail.com>
2018-05-11 22:59:55 -0700

    next-sample control complete

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-/* global $, document */
+/* global $, document, MathJax */
 
 /* net parameters */
 function changeNumLayers() { //eslint-disable-line no-unused-vars
@@ -16,10 +16,9 @@ function changeNumLayers() { //eslint-disable-line no-unused-vars
 
 function changeNonLinearity() { //eslint-disable-line no-unused-vars
     var x = document.getElementById("nonlin-type").value;
-    console.log(x);
     var nonLins = [document.getElementById("net-1-nonlin-1"),
-                      document.getElementById("net-2-nonlin-1"),
-                      document.getElementById("net-2-nonlin-2")];
+                   document.getElementById("net-2-nonlin-1"),
+                   document.getElementById("net-2-nonlin-2")];
     for (var i=0; i<nonLins.length; i++) {
         if (x === "linear") {
             nonLins[i].src = "images/linear.png";
@@ -32,28 +31,61 @@ function changeNonLinearity() { //eslint-disable-line no-unused-vars
 }
 
 /* control buttons */
+function nextSample() { //eslint-disable-line no-unused-vars
+    var sample = globalData[Math.floor(Math.random()*globalData.length)];
+    var sampleX = sample.slice(0, 4);
+    var sampleY = [0.0, 0.0, 0.0];
+    sampleY[sample[4]] = 1.0;
+    
+    /* Net-1 sample */
+    var net1math = MathJax.Hub.getAllJax("net-1");
+    MathJax.Hub.Queue(["Text", net1math[0], vector2Str(sampleX)]);
+    MathJax.Hub.Queue(["Text", net1math[net1math.length-2], vector2Str(sampleY)]);
+    
+    /* Net-2 sample */
+    var net2math = MathJax.Hub.getAllJax("net-2");
+    MathJax.Hub.Queue(["Text", net2math[0], vector2Str(sampleX)]);
+    MathJax.Hub.Queue(["Text", net2math[net2math.length-2], vector2Str(sampleY)]);
+}
 
-$(document).ready(function() {
-    'use strict';
-    $('#next-input').click(function(event) { //eslint-disable-line no-unused-vars
-        'use strict';
-        // remove 'active' from all li
-        $('.navbar-nav li a').parent().removeClass("active");
-        // add 'active' to specific li
-        $(this).parent().addClass("active");
-    })
+/* helper functions */
+function isNet1() { //eslint-disable-line no-unused-vars
+    return document.getElementById("num-layers").value() === "1";
+}
+
+function isNet2() { //eslint-disable-line no-unused-vars
+    return document.getElementById("num-layers").value() === "2";
+}
+
+function vector2Str(vector) {
+    var s = "\\begin{bmatrix}";
+    for (var i=0; i<vector.length; i++) {
+        s = s.concat(vector[i]);
+        s = s.concat("\\\\");
+    }
+    s = s.concat("\\end{bmatrix}");
+    return s;
+}
+    
+function matrix2Str(matrix) {
+    
+}
+
+var globalData = [];
+var iris2index = {"Iris-setosa":0, "Iris-versicolor":1, "Iris-virginica":2};
+
+/* Load data for training and testing */
+$.ajax({
+    url: "iris-data.txt",
+    success: function(data) { 
+        var lines = data.split(/\r?\n/);
+        for (var i=0; i<lines.length; i++) {
+            var line = lines[i].split(",");
+            if (line.length === 5) {
+                var sample = line.slice(0, 4);
+                sample.push(iris2index[line[4]]);
+                globalData.push(sample);
+            }
+        }
+    }
 });
-
-$(document).ready(function() {
-    'use strict';
-    $('#next-input').click(function(event) { //eslint-disable-line no-unused-vars
-        'use strict';
-        // remove 'active' from all li
-        $('.navbar-nav li a').parent().removeClass("active");
-        // add 'active' to specific li
-        $(this).parent().addClass("active");
-    })
-});
-
-
-
